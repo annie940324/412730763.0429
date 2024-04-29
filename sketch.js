4417c1d6cc91f20fa96dba4333d59164f8a30cdf
annie940324 <150638771+annie940324@users.noreply.github.com>
2024-04-29 10:40:34 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,17 @@
 var captureGraphics
-var capture_width=640
-var capture_height=480
-var span =5
+var capture_width = 640
+var capture_height = 480
+var span = 5
 function setup() {
   createCanvas(windowWidth, windowHeight);
   capture = createCapture(VIDEO)
-  capture.size(capture_height,capture_height);//設定顯示畫面大小
-  captureGraphics = captureGraphics(capture_width,capture_height)
-  captureGraphics=translate(capture_width,0)
+  capture.size(capture_width,capture_height)
+  captureGraphics = createGraphics(capture_width,capture_height)
+  captureGraphics.translate(capture_width,0)
   captureGraphics.scale(-1,1)
-   captureGraphics.hide()
-   //
+  captureGraphics.hide()
+ 
+
 }
 
 function draw() {
@@ -18,14 +19,13 @@ function draw() {
   noStroke()
   span = 5+map(mouseX,0,width,0,20)
   push()
-  translate(width/2-capture_width/2, height/2-capture_height) //把原點移到(width/2-160, height/2-120)
-  captureGraphics.image(capture,0,0) // 在0,0
-  for(var x = 0;x<captureGraphics.width;x=x+span){
-    for(var x = 0;y<captureGraphics.height;y=y+span){
+  translate(width/2-capture_width/2, height/2-capture_height/2) //把原點移到(width/2-160, height/2-120)
+  captureGraphics.image(capture,0,0) //
+  for(var x= 0;x<captureGraphics.width;x=x+10){
+    for(var y=0;y<captureGraphics.height;y=y+10){
       var pixel = captureGraphics.get(x,y)
       fill(pixel)
-      rect(x,y,span)
-      ellipse(x,y,span)
+      rect(x,y,10)
     }
   }
   pop()
